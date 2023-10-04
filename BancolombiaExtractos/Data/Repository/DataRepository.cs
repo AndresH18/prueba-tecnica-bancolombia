f14172bd7cfe2c79c69363316e4068edd065f072
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-10-03 20:33:04 -0500

    Repository

---
+++ BancolombiaExtractos/Data/Repository/DataRepository.cs
@@ -0,0 +1,54 @@
+﻿using BancolombiaExtractos.Data.Models;
+using BancolombiaExtractos.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancolombiaExtractos.Data.Repository;
+
+public class DataRepository : IRepository
+{
+    private readonly SemaphoreSlim _semaphore = new(1);
+    private readonly PruebaBancolombiaContext _db;
+
+    public DataRepository(PruebaBancolombiaContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Result<bool>> IsCredentialsValid(string email, int accountId)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            var account = await _db.Cuentas.Include(c => c.Usuario)
+                .Where(c => c.NumeroCuenta == accountId && c.Usuario.Email.Equals(email)).FirstOrDefaultAsync();
+
+            return account is not null;
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task<Result<IEnumerable<Movimiento>>> GetMovimientosExtracto(int accountId)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            return await _db.Movimientos.Where(m => m.NumeroCuenta == accountId && m.Fecha.Month == DateTime.Now.Month)
+                .ToListAsync();
+        }
+        catch
+        {
+            return new Result<IEnumerable<Movimiento>>(Enumerable.Empty<Movimiento>());
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
---
+++ BancolombiaExtractos/Data/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using BancolombiaExtractos.Data.Models;
+using BancolombiaExtractos.Helpers;
+
+namespace BancolombiaExtractos.Data.Repository;
+
+public interface IRepository
+{
+    public Task<Result<bool>> IsCredentialsValid(string email, int accountId);
+    public Task<Result<IEnumerable<Movimiento>>> GetMovimientosExtracto(int accountId);
+}
