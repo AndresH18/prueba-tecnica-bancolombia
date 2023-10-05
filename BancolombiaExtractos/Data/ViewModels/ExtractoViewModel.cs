084855f5f67370106fef41ce21a84edc2b2d1a7e
andre <andres.hoyos18@eia.edu.co>
2023-10-05 10:29:42 -0500

    Updated data annotation error messages

--- BancolombiaExtractos/Data/ViewModels/ExtractoViewModel.cs
+++ BancolombiaExtractos/Data/ViewModels/ExtractoViewModel.cs
@@ -4,6 +4,9 @@ namespace BancolombiaExtractos.Data.ViewModels;
 
 public class ExtractoViewModel
 {
-    [EmailAddress] public string Email { get; set; } = string.Empty;
-    [Range(1, int.MaxValue)] public int Account { get; set; } 
+    [EmailAddress(ErrorMessage = "Se debe ingresar un correo")]
+    public string Email { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue, ErrorMessage = "Ingrese un numero de cuenta valido")]
+    public int Account { get; set; }
 }
